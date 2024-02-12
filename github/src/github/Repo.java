fc17cbe56dd029dddb040c6abddbdbfa3b5c1d3b
Shreya2-22 <kshreya22@tbc.edu.np>
2024-02-12 17:16:22 +0545

    initial commit

---
+++ github/src/github/Repo.java
@@ -0,0 +1,15 @@
+package github;
+
+public class Repo
+{
+
+
+	public static void main(String[] args)
+	{
+		System.out.println("Hello world my name is shreya khadka");
+		// TODO Auto-generated method stub
+
+	}
+	
+
+}
